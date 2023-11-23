bef03afb8b4a49ff45f2dd3d98858da209f5935b
Lívio Lopes <antonio.lvio760@gmail.com>
2023-11-23 17:07:28 -0300

    feat(language_model): implements classmethod list_dicts on LanguageModel

--- src/models/language_model.py
+++ src/models/language_model.py
@@ -5,9 +5,13 @@ from database.db import db
 class LanguageModel(AbstractModel):
     _collection = db["languages"]
 
-    def __init__(self, data: dict):
+    def __init__(self, data: dict[str, str]):
         super().__init__(data)
 
-    def to_dict(self):
+    def to_dict(self) -> dict[str, str]:
         del self.data["_id"]
         return self.data
+
+    @classmethod
+    def list_dicts(cls) -> list[dict[str, str]]:
+        return [language.to_dict() for language in cls.find()]
