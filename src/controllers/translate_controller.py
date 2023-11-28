d4ed323fc80fd085aac30e425d29d7f30bb16944
Lívio Lopes <antonio.lvio760@gmail.com>
2023-11-28 10:22:07 -0300

    feat(translate_controller): implements method POST for translate text

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -1,6 +1,6 @@
 from models.language_model import LanguageModel
-from flask import render_template, Blueprint
-
+from flask import render_template, Blueprint, request
+from deep_translator import GoogleTranslator
 
 translate_controller = Blueprint("translate", __name__)
 
@@ -13,6 +13,25 @@ def index():
         "translate_from": "pt",
         "translate_to": "en",
         "translated": "What do you want to translate?",
+    }
+
+    return render_template("index.html", **data)
+
+
+@translate_controller.post("/")
+def translate():
+    text_to_translate = request.form.get('text-to-translate')
+    translate_from = request.form.get('translate-from')
+    translate_to = request.form.get('translate-to')
+    translated = GoogleTranslator(
+        source=translate_from or 'auto', target=translate_to
+    ).translate(text_to_translate)
+    data = {
+        "languages": LanguageModel.list_dicts(),
+        "text_to_translate": text_to_translate,
+        "translate_from": translate_from,
+        "translate_to": translate_to,
+        "translated": translated,
     }
 
     return render_template("index.html", **data)
